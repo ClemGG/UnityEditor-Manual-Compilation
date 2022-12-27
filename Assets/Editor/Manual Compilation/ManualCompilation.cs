54e29ce951d605649d817ae044dcdb2db5d4000b
Clement Guichard <clement.guichard@e-artsup.net>
2022-12-27 18:20:54 +0100

    - Ajout documentation au code

--- Assets/Editor/Manual Compilation/ManualCompilation.cs
+++ Assets/Editor/Manual Compilation/ManualCompilation.cs
@@ -5,42 +5,53 @@ using UnityEditor.Compilation;
  using System.Reflection;
 #endif
 using UnityEngine;
+
+// https://github.com/marijnz/unity-toolbar-extender
 using UnityToolbarExtender;
 
 namespace Project.Editor
 {
+    /// <summary>
+    /// InitializeOnLoad appelera automatiquement le constructeur
+    /// quand Unity s'ouvre
+    /// </summary>
     [InitializeOnLoad]
     public class ManualCompilation
     {
+        /// <summary>
+        /// Appelé auto. par InitializeOnLoad
+        /// ou quand on recompile manuellement
+        /// </summary>
         static ManualCompilation()
         {
+            // Extension pour ajouter des boutons dans la Toolbar d'Unity
+            // avant ou après les boutons pour lancer le mode Play
             ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+
+            // Empêche la recompilation automatique
             EditorApplication.LockReloadAssemblies();
         }
 
-        ~ManualCompilation()
-        {
-            ToolbarExtender.LeftToolbarGUI.Remove(OnToolbarGUI);
-            EditorApplication.UnlockReloadAssemblies();
-        }
-
+        /// <summary>
+        /// S'abonne au ToolbarExtender pour créer des boutons
+        /// à côté des boutons du mode Play
+        /// </summary>
         private static void OnToolbarGUI()
         {
             GUILayout.FlexibleSpace();
 
+            // Relance la compilation manuellement depuis un bouton dans la Toolbar d'Unity
             if (GUILayout.Button(new GUIContent("R", "Recompile"), EditorStyles.toolbarButton, GUILayout.MinWidth(30)))
             {
                 EditorApplication.UnlockReloadAssemblies();
                 Recompile();
             }
-            //if (GUILayout.Button(new GUIContent("u", "unlock"), EditorStyles.toolbarButton, GUILayout.MinWidth(30)))
-            //{
-            //    EditorApplication.UnlockReloadAssemblies();
-            //}
 
         }
 
-
+        /// <summary>
+        /// Recompile les scripts manuellement
+        /// </summary>
         private static void Recompile()
         {
 #if UNITY_2019_3_OR_NEWER
@@ -52,12 +63,5 @@ namespace Project.Editor
                  dirtyAllScriptsMethod.Invoke(editorCompilationInterfaceType, null);
 #endif
         }
-
-        [UnityEditor.Callbacks.DidReloadScripts]
-        private static void OnCompilationFinished()
-        {
-            //EditorApplication.LockReloadAssemblies();
-        }
-
     }
 }
